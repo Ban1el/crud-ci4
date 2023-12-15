b51b21664a963e5a505192e5017cbc2e65aa736d
Ban1el <danieltroyaromin1337@gmail.com>
2023-12-15 10:43:25 +0800

    add login check filter

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -8,14 +8,17 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'Home::index');
 
 //Students
-$routes->get('students/dashboard', 'StudentController::index');
-$routes->get('students/create_student', 'StudentController::create_student');
+$routes->get('login', 'LoginController::index');
+$routes->post('login_user', 'LoginController::login_user');
 $routes->post('students/add_student', 'StudentController::add_student');
-$routes->get('students/edit_student/(:segment)', 'StudentController::edit_student/$1');
 $routes->put('students/modify_student/(:segment)', 'StudentController::modify_student/$1');
 $routes->delete('students/delete_student/(:segment)', 'StudentController::delete_student/$1');
 $routes->post('students/export_to_excel', 'StudentController::export_to_excel');
-$routes->get('register', 'RegisterController::index');
 $routes->post('register_user', 'RegisterController::register_user');
-$routes->get('login', 'LoginController::index');
-$routes->post('login_user', 'LoginController::login_user');
+
+$routes->group('', ['filter' => 'LoginCheck'], function ($routes) {
+    $routes->get('students/dashboard', 'StudentController::index');
+    $routes->get('students/create_student', 'StudentController::create_student');
+    $routes->get('students/edit_student/(:segment)', 'StudentController::edit_student/$1');
+    $routes->get('register', 'RegisterController::index');
+});
